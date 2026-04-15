4941a153c9f74bcfc63724cb60fc8442e0036bf9
Farha Tazmeen C <cfarhatazmeen@gmail.com>
2026-04-15 09:28:32 +0530

    Implement canCompleteCircuit method in leet134

---
+++ DAY-12/leet134.java
@@ -0,0 +1,15 @@
+class leet134 {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int tank = 0, total = 0, start = 0;
+        for(int i=0; i<cost.length; i++){
+            int diff = gas[i] - cost[i];
+            total += diff;
+            tank += diff;
+            if(tank<0){
+                start=i+1;
+                tank=0;
+            }
+        }
+        return total>=0? start:-1;
+    }
+}
